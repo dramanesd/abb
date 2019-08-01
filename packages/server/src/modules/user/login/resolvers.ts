32a3fba39948c02a780da55328d9ca9f8f15a687
DramaneD <ddramane63@gmail.com>
2019-08-01 07:27:51 +0000

    Set up the returne of sessionId after login

--- packages/server/src/modules/user/login/resolvers.ts
+++ packages/server/src/modules/user/login/resolvers.ts
@@ -26,31 +26,31 @@ export const resolvers: ResolverMap = {
       const user = await User.findOne({ where: { email } });
 
       if (!user) {
-        return errorResponse;
+        return {errors: errorResponse };
       }
 
       if (!user.confirmed) {
-        return [
+        return { errors: [
           {
             path: "email",
             message: confirmEmailError
           }
-        ];
+        ] };
       }
 
       if (user.forgotPasswordLocked) {
-        return [
+        return { errors: [
           {
             path: "email",
             message: forgotPasswordLockedError
           }
-        ];
+        ] };
       }
 
       const valid = await bcrypt.compare(password, user.password);
 
       if (!valid) {
-        return errorResponse;
+        return { errors: errorResponse };
       }
 
       // login sucessful
@@ -59,7 +59,7 @@ export const resolvers: ResolverMap = {
         await redis.lpush(`${userSessionIdPrefix}${user.id}`, req.sessionID);
       }
 
-      return null;
+      return { sessionId: req.sessionID };
     }
   }
 };
