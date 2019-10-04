7fc95a71944f1c59556b4ff992bcce0606e0e67e
DramaneD <ddramane63@gmail.com>
2019-10-04 22:13:49 +0000

    Implement forgot password logic

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -1,6 +1,10 @@
 import * as nodemailer from "nodemailer";
 
-export const sendEmail = async (recipient: string, url: string) => {
+export const sendEmail = async (
+  recipient: string,
+  url: string,
+  linkText: string
+) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
@@ -20,7 +24,7 @@ export const sendEmail = async (recipient: string, url: string) => {
     html: `<html>
         <body>
         <p>Testing NodeMail - the world's most awesomest email service!</p>
-        <a href="${url}">confirm email</a>
+        <a href="${url}">${linkText}</a>
         </body>
         </html>`
   };
--- packages/web/src/modules/forgotPassword/ForgotPasswordConnector.tsx
+++ packages/web/src/modules/forgotPassword/ForgotPasswordConnector.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { ForgotPasswordView } from "./ui/ForgotPasswordView";
+import { ForgotPasswordController } from "@abb/controller";
 
 export class ForgotPasswordConnector extends React.PureComponent {
-  dummy = async (values: any) => {
-    console.log(values);
-    return null;
-  };
-
   render() {
-    return <ForgotPasswordView submit={this.dummy} />;
+    return (
+      <ForgotPasswordController>
+        {({ submit }) => <ForgotPasswordView submit={submit} />}
+      </ForgotPasswordController>
+    );
   }
 }
